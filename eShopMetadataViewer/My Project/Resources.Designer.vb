70bce600d96f8869de961205243b655353769a06
Lanausse <lanausse1@iCloud.com>
2022-03-03 08:22:04 -0500

    TopScreenBG

--- eShopMetadataViewer/My Project/Resources.Designer.vb
+++ eShopMetadataViewer/My Project/Resources.Designer.vb
@@ -89,5 +89,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property TopScreenBG_ext() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("TopScreenBG.ext", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace

1ca7d39e3cfa0da5e202cc55dd074d37769f37fc
Kai Lanausse <66651363+KLanausse@users.noreply.github.com>
2023-02-14 13:39:30 -0500

    Start of C# rewrite

---
+++ eShopListingViewer/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace eShopListingViewer
+{
+    partial class Viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Viewer));
+            this.BottomScreen = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // BottomScreen
+            // 
+            this.BottomScreen.BackColor = System.Drawing.Color.Ivory;
+            this.BottomScreen.Font = new System.Drawing.Font("FOT-RodinNTLG Pro DB", 12F, System.Drawing.FontStyle.Bold);
+            this.BottomScreen.Location = new System.Drawing.Point(40, 264);
+            this.BottomScreen.Name = "BottomScreen";
+            this.BottomScreen.Size = new System.Drawing.Size(320, 240);
+            this.BottomScreen.TabIndex = 0;
+            // 
+            // Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(400, 504);
+            this.Controls.Add(this.BottomScreen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(416, 543);
+            this.MinimumSize = new System.Drawing.Size(416, 543);
+            this.Name = "Viewer";
+            this.Text = "eShop Listing Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BottomScreen;
+    }
+}
+
---
+++ eShopListingViewer/Forms/TopScreen/InfoESBR.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace eShopListingViewer.Forms
+{
+    partial class InfoESBR
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.N_root_00 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // N_root_00
+            // 
+            this.N_root_00.Location = new System.Drawing.Point(185, 100);
+            this.N_root_00.Name = "N_root_00";
+            this.N_root_00.Size = new System.Drawing.Size(30, 40);
+            this.N_root_00.TabIndex = 0;
+            // 
+            // InfoESBR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.N_root_00);
+            this.Font = new System.Drawing.Font("FOT-RodinNTLG Pro DB", 12.75F, System.Drawing.FontStyle.Bold);
+            this.Name = "InfoESBR";
+            this.Size = new System.Drawing.Size(400, 240);
+            this.Load += new System.EventHandler(this.InfoESBR_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel N_root_00;
+    }
+}
